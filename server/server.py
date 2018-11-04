b7ed54a83831a36acf5085814e0a7660f4cf69cd
karaage0703 <coldchicken@gmail.com>
2018-11-04 22:19:43 +0900

    Add keyboard interrupt for fix socket error

--- server/server.py
+++ server/server.py
@@ -12,33 +12,36 @@ serversock.bind((host,port)) #IPとPORTを指定してバインドします
 serversock.listen(10) #接続の待ち受けをします（キューの最大数を指定）
 
 print('Waiting for connections...')
-clientsock, client_address = serversock.accept() #接続されればデータを格納
+try:
+    clientsock, client_address = serversock.accept() #接続されればデータを格納
 
-while True:
-    rcvmsg = clientsock.recv(1024)
-    print('Received -> %s' % (rcvmsg))
-    if rcvmsg == '.':
-        break
-    if rcvmsg == b'go':
-        print('go')
-        clientsock.send(b'ok')
-        photo_name = clientsock.recv(1024)
-        photo_name = photo_name.decode()
-        print("photo_name=" + photo_name)
-        print ("uploading...")
-        cmd = "./download.sh " + photo_name
-        subprocess.call(cmd, shell=True)
+    while True:
+        rcvmsg = clientsock.recv(1024)
+        print('Received -> %s' % (rcvmsg))
+        if rcvmsg == '.':
+            break
+        if rcvmsg == b'go':
+            print('go')
+            clientsock.send(b'ok')
+            photo_name = clientsock.recv(1024)
+            photo_name = photo_name.decode()
+            print("photo_name=" + photo_name)
+            print ("uploading...")
+            cmd = "./download.sh " + photo_name
+            subprocess.call(cmd, shell=True)
 
-        print("processing...")
-        cmd = "./deeplearning.sh " + photo_name
-        subprocess.call(cmd, shell=True)
+            print("processing...")
+            cmd = "./deeplearning.sh " + photo_name
+            subprocess.call(cmd, shell=True)
 
-        clientsock.send(b'processed')
-        time.sleep(1.0)
+            clientsock.send(b'processed')
+            time.sleep(1.0)
 
-        print("downloading...")
-        cmd = "./upload.sh "
-        subprocess.call(cmd, shell=True)
-        clientsock.send(b'uploaded')
+            print("downloading...")
+            cmd = "./upload.sh "
+            subprocess.call(cmd, shell=True)
+            clientsock.send(b'uploaded')
 
-clientsock.close()
+except KeyboardInterrupt:
+    print('Interrupt!')
+    serversock.close()
